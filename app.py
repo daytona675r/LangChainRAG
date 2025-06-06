0e59f3a662da805d13f26aa85ebc10b4a1d8f6ae
daytona675r <mario.wangen@live.de>
2025-06-06 10:50:04 +0200

    ADD: indexing part of the pipeline - parsing, loading, splitting, embedding and storing in the vector store

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 #environment variables
@@ -26,4 +29,30 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 # Vector store initialization
 vector_store = InMemoryVectorStore(embeddings)
 
+# Only keep post title, headers, and content from the full HTML.
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+docs = loader.load()
+
+assert len(docs) == 1
+print(f"Total characters: {len(docs[0].page_content)}")
+print(docs[0].page_content[:500])
+
+# Split the document into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # chunk size (characters)
+    chunk_overlap=200,  # chunk overlap (characters)
+    add_start_index=True,  # track index in original document
+)
+all_splits = text_splitter.split_documents(docs)
+
+print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+# Add the split documents to the vector store
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
 
